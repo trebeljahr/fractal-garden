b5532c454c5095f5f6383b55500f01f4f8f53478
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2022-09-27 12:27:28 +0200

    Edit barnsley fern to support more ferns

--- pages/barnsley-fern.tsx
+++ pages/barnsley-fern.tsx
@@ -13,13 +13,12 @@ const Sketch = dynamic(() => import("react-p5").then((mod) => mod.default), {
 let config: Configuration;
 let x = 0;
 let y = 0;
-let iterations = 1;
 
 class Configuration {
   public detail = 60;
   public color = "#96f78e";
   public background = "#252424";
-  public useBarnsley = true;
+  public fernToUse = matrices.tree;
   // public name = "img_name";
   // save(p5: P5) {
   //   p5.saveCanvas(pg, this.name, "jpg");
@@ -32,24 +31,21 @@ function applyMatrixValues(xValue: number, yValue: number, matrix: number[]) {
   return { newXValue, newYValue };
 }
 
-function generateNewCoords(
-  xValue: number,
-  yValue: number,
-  matrices: number[][]
-) {
+function generateNewCoords(xValue: number, yValue: number) {
   const r = Math.random();
-  const prob1 = matrices[1][6];
-  const prob2 = matrices[2][6];
-  const prob3 = matrices[3][6];
-  const prob4 = matrices[0][6];
+  const matrix = config.fernToUse.matrix;
+  const prob1 = matrix[1][6];
+  const prob2 = matrix[2][6];
+  const prob3 = matrix[3][6];
+  const prob4 = matrix[0][6];
   if (r <= prob1) {
-    return applyMatrixValues(xValue, yValue, matrices[1]);
+    return applyMatrixValues(xValue, yValue, matrix[1]);
   } else if (r <= prob1 + prob2) {
-    return applyMatrixValues(xValue, yValue, matrices[2]);
+    return applyMatrixValues(xValue, yValue, matrix[2]);
   } else if (r <= prob1 + prob2 + prob3) {
-    return applyMatrixValues(xValue, yValue, matrices[3]);
+    return applyMatrixValues(xValue, yValue, matrix[3]);
   } else if (r <= prob1 + prob2 + prob3 + prob4) {
-    return applyMatrixValues(xValue, yValue, matrices[0]);
+    return applyMatrixValues(xValue, yValue, matrix[0]);
   }
   return {
     newXValue: 0,
@@ -57,33 +53,74 @@ function generateNewCoords(
   };
 }
 
-function getDrawX(p5: P5) {
-  const low = p5.width / 2 - p5.height / 3;
-  const high = p5.width / 2 + p5.height / 3;
-  return config.useBarnsley
-    ? Math.floor(p5.map(x, -2.182, 2.6558, low, high))
-    : Math.floor(p5.map(x, -1.4, 1.7558, low, high));
-}
+const barnsley = {
+  matrix: [
+    [0, 0, 0, 0.16, 0, 0, 0.01],
+    [0.85, 0.04, -0.04, 0.85, 0, 1.6, 0.85],
+    [0.2, -0.26, 0.23, 0.22, 0, 1.6, 0.07],
+    [-0.15, 0.28, 0.26, 0.24, 0, 0.44, 0.07],
+  ],
+  scaleFactor: 1,
+};
 
-function getDrawY(p5: P5) {
-  return config.useBarnsley
-    ? Math.floor(p5.map(y, 0, 9.9983, p5.height, 100))
-    : Math.floor(p5.map(y, -0.5, 6.9983, p5.height, 200));
-}
+const cyclosorus = {
+  matrix: [
+    [0, 0, 0, 0.25, 0, -0.4, 0.02],
+    [0.95, 0.005, -0.005, 0.93, -0.002, 0.5, 0.84],
+    [0.035, -0.2, 0.16, 0.04, -0.09, 0.02, 0.07],
+    [-0.04, 0.2, 0.16, 0.04, 0.083, 0.12, 0.07],
+  ],
+  scaleFactor: 2,
+};
 
-const barnsleyMatrices = [
-  [0, 0, 0, 0.16, 0, 0, 0.01],
-  [0.85, 0.04, -0.04, 0.85, 0, 1.6, 0.85],
-  [0.2, -0.26, 0.23, 0.22, 0, 1.6, 0.07],
-  [-0.15, 0.28, 0.26, 0.24, 0, 0.44, 0.07],
-];
+const modifiedBarnsley = {
+  matrix: [
+    [0, 0, 0, 0.2, 0, -0.12, 0.01],
+    [0.845, 0.035, -0.035, 0.82, 0, 1.6, 0.85],
+    [0.2, -0.31, 0.255, 0.245, 0, 0.29, 0.07],
+    [-0.15, 0.24, 0.25, 0.2, 0, 0.68, 0.07],
+  ],
+  scaleFactor: 1,
+};
 
-const differentMatrices = [
-  [0, 0, 0, 0.25, 0, -0.4, 0.02],
-  [0.95, 0.005, -0.005, 0.93, -0.002, 0.5, 0.84],
-  [0.035, -0.2, 0.16, 0.04, -0.09, 0.02, 0.07],
-  [-0.04, 0.2, 0.16, 0.04, 0.083, 0.12, 0.07],
-];
+const culcita = {
+  matrix: [
+    [0, 0, 0, 0.25, 0, -0.14, 0.02],
+    [0.85, 0.02, -0.02, 0.83, 0, 1, 0.84],
+    [0.09, -0.28, 0.3, 0.11, 0, 0.6, 0.07],
+    [-0.09, 0.28, 0.3, 0.09, 0, 0.7, 0.07],
+  ],
+  scaleFactor: 2,
+};
+
+const fishbone = {
+  matrix: [
+    [0, 0, 0, 0.25, 0, -0.4, 0.02],
+    [0.95, 0.002, -0.002, 0.93, -0.002, 0.5, 0.84],
+    [0.035, -0.11, 0.27, 0.01, -0.05, 0.005, 0.07],
+    [-0.04, 0.11, 0.27, 0.01, 0.047, 0.06, 0.07],
+  ],
+  scaleFactor: 2,
+};
+
+const tree = {
+  matrix: [
+    [0, 0, 0, 0.5, 0, 0, 0.05],
+    [0.42, -0.42, 0.42, 0.42, 0, 0.2, 0.4],
+    [0.42, 0.42, -0.42, 0.42, 0, 0.2, 0.4],
+    [0.1, 0, 0, 0.1, 0, 0.2, 0.15],
+  ],
+  scaleFactor: 24,
+};
+
+const matrices = {
+  tree,
+  fishbone,
+  culcita,
+  modifiedBarnsley,
+  cyclosorus,
+  barnsley,
+};
 
 type Props = {
   description: string;
@@ -96,7 +133,9 @@ const BarnsleyFern = ({ description }: Props) => {
       canvasParentRef
     );
     p5.background(config.background);
-    iterations = 1;
+    // p5.angleMode(p5.DEGREES);
+    // p5.rotate(180);
+
     // const gui = new dat.GUI();
     // const o = gui.addFolder("Options");
     // const iterationController = o.add(config, "detail", 10, 80).step(1);
@@ -113,29 +152,27 @@ const BarnsleyFern = ({ description }: Props) => {
     // saving.add(config, "save");
   };
 
+  const draw = (p5: P5) => {
+    for (let i = 0; i < 1000; i++) {
+      p5.stroke(config.color);
+      const fernHeight = p5.height * config.fernToUse.scaleFactor;
+      const fernWidth = fernHeight / 2;
+
+      let plotX = fernWidth * ((x + 3) / 6) + p5.width / 2 - fernWidth / 2;
+      let plotY = p5.height - (fernHeight * y) / 14;
+
+      p5.strokeWeight(1);
+      p5.point(plotX, plotY);
+
+      const coords = generateNewCoords(x, y);
+      x = coords.newXValue;
+      y = coords.newYValue;
+    }
+  };
+
   const windowResized = (p5: P5) => {
     p5.resizeCanvas(window.innerWidth, window.innerHeight);
     p5.background(config.background);
-    iterations = 1;
-  };
-
-  const draw = (p5: P5) => {
-    if (iterations < config.detail) {
-      iterations++;
-      for (let i = 0; i < 10000; i++) {
-        p5.stroke(config.color);
-        const drawX = getDrawX(p5);
-        const drawY = getDrawY(p5);
-        p5.strokeWeight(40 / config.detail);
-        p5.point(drawX, drawY);
-        const matrices = config.useBarnsley
-          ? barnsleyMatrices
-          : differentMatrices;
-        const coords = generateNewCoords(x, y, matrices);
-        x = coords.newXValue;
-        y = coords.newYValue;
-      }
-    }
   };
 
   return (
