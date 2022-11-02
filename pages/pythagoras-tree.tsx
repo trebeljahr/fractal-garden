a802ba9ced2cd60cf1cbf6de96d96ca2d4bce5ab
npx <>
2022-11-02 20:08:11 +0100

    feat(pythagoras-tree): add option to fill squares

--- pages/pythagoras-tree.tsx
+++ pages/pythagoras-tree.tsx
@@ -22,6 +22,7 @@ type Config = {
   angle: number;
   background: string;
   fillTriangles: boolean;
+  fillSquares: boolean;
 };
 
 type Props = {
@@ -65,6 +66,7 @@ const PythagorasTreeComponent = ({ description }: Props) => {
     angle: 45,
     background: "#252424",
     fillTriangles: true,
+    fillSquares: true,
   });
 
   const { width, height } = useWindowSize();
@@ -86,7 +88,7 @@ const PythagorasTreeComponent = ({ description }: Props) => {
   useEffect(() => {
     if (!ctx || !width || !height) return;
 
-    const drawPoly = (points: Vec2D[], color: string) => {
+    const drawPoly = (points: Vec2D[], color: string, fill = true) => {
       const [start, ...remaining] = points;
 
       ctx.beginPath();
@@ -95,7 +97,7 @@ const PythagorasTreeComponent = ({ description }: Props) => {
       ctx.closePath();
 
       ctx.fillStyle = color;
-      ctx.fill();
+      if (fill) ctx.fill();
       ctx.strokeStyle = color;
       ctx.stroke();
     };
@@ -112,12 +114,12 @@ const PythagorasTreeComponent = ({ description }: Props) => {
       const p4 = Vector.sub(p1, d);
 
       const color = hsvGradient(depth);
-      drawPoly([p1, p2, p3, p4], color);
+      drawPoly([p1, p2, p3, p4], color, config.fillSquares);
 
       if (depth === config.iterations) return;
 
       const p5 = thirdPoint(p3, p4);
-      if (config.fillTriangles) drawPoly([p3, p4, p5], color);
+      drawPoly([p3, p4, p5], color, config.fillTriangles);
 
       drawBranch(p4, p5, depth + 1);
       drawBranch(p5, p3, depth + 1);
@@ -170,6 +172,7 @@ const PythagorasTreeComponent = ({ description }: Props) => {
             />
             <DatBoolean path="animateIterations" label="animate" />
             <DatBoolean path="fillTriangles" label="fill triangles" />
+            <DatBoolean path="fillSquares" label="fill squares" />
           </DatFolder>
         </DatGui>
 
