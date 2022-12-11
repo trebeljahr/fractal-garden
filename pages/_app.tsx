2734e3908d7045e81348f7a963539daa1d113755
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2022-12-11 23:51:24 +0100

    Add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,25 @@ import "../styles/react-dat-gui.css";
 
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-5EP0KS9R28"
+        />
+        <Script id="gtaginit">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-5EP0KS9R28');
+          `}
+        </Script>
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, minimum-scale=1"
