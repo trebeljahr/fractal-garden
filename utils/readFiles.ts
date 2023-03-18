169a1de33b409140fee74c4a663f0e1d77c37100
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2023-03-18 10:10:25 +0100

    Delete everything

--- utils/readFiles.ts
+++
@@ -1,9 +0,0 @@
-import { readFile } from "fs/promises";
-import { join } from "path";
-
-export function getDescription(fileName: string) {
-  return readFile(
-    join(process.cwd(), "fractal-descriptions", fileName),
-    "utf-8"
-  );
-}
--- utils/shaders/mandelbrot.vert
+++
@@ -1,9 +0,0 @@
-precision highp float;
-attribute vec3 aPosition;
-
-void main() {
-    vec4 positionVec4 = vec4(aPosition, 1.0);
-    positionVec4.xy = positionVec4.xy * 2.0 - 1.0;
-    
-    gl_Position = positionVec4;
-}
